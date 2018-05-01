826752ff8610dba6ba4f38d6184884b7461a8214
vonavi <ivvl82@gmail.com>
2018-05-01 11:46:09 +0300

    Mask-outputs layer implemented.

---
+++ external/caffe/include/caffe/layers/mask_outputs_layer.hpp
@@ -0,0 +1,33 @@
+#ifndef CAFFE_MASK_OUTPUTS_LAYER_HPP_
+#define CAFFE_MASK_OUTPUTS_LAYER_HPP_
+
+#include <vector>
+
+#include "caffe/blob.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/proto/caffe.pb.h"
+
+namespace caffe {
+
+template <typename Dtype>
+class MaskOutputsLayer : public Layer<Dtype> {
+ public:
+  explicit MaskOutputsLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "MaskOutputs"; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
+}  // namespace caffe
+
+#endif  // CAFFE_MASK_OUTPUTS_LAYER_HPP_
